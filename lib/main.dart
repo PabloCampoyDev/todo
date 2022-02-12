2b25b0238f02efde33d9e4ddb8ceddf1aa09e9c7
pablo_campoy <pablo@orasystems.com.br>
2022-02-12 15:33:25 -0300

    salvando e listando tarefas com shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'models/item.dart';
 
 void main() => runApp(const MyApp());
@@ -26,41 +28,115 @@ class HomePage extends StatefulWidget {
 
   var items = <Item>[];
 
-  HomePage({Key? key}) : super(key: key) {
-    items = [];
-    items.add(Item(title: "Banana", done: false));
-    items.add(Item(title: "Maça", done: true));
-    items.add(Item(title: "Abacaxi", done: false));
-  }
+  //HomePage({Key? key}) : super(key: key) {
+  //items = [];
+  // items.add(Item(title: "Banana", done: false));
+  // items.add(Item(title: "Maça", done: true));
+  // items.add(Item(title: "Abacaxi", done: false));
+  //}
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  var newTaskControler = TextEditingController();
+
+  void add() {
+    if (newTaskControler.text.isEmpty) return;
+    setState(
+      () {
+        widget.items.add(
+          Item(
+            title: newTaskControler.text,
+            done: false,
+          ),
+        );
+        newTaskControler.text = "";
+        save();
+      },
+    );
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decode = jsonDecode(data);
+      List<Item> result = decode.map((x) => Item.fromJson(x)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  Future save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState() {
+    load();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo list"),
+        title: TextFormField(
+          controller: newTaskControler,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          decoration: InputDecoration(
+            labelText: "Nova tarefa",
+            labelStyle: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
       body: ListView.builder(
         itemCount: widget.items.length,
         itemBuilder: (BuildContext context, int index) {
           final item = widget.items[index];
-          return CheckboxListTile(
-            title: Text(item.title!),
+          return Dismissible(
             key: Key(item.title!),
-            value: item.done,
-            onChanged: (value) {
-              setState(
-                () {
-                  item.done = value;
-                },
-              ); // setState
-            }, // onChanged
+            background: Container(
+              color: Colors.red.withOpacity(0.2),
+            ),
+            child: CheckboxListTile(
+              title: Text(item.title!),
+              value: item.done,
+              onChanged: (value) {
+                setState(
+                  () {
+                    item.done = value;
+                    save();
+                  },
+                );
+              },
+            ),
+            onDismissed: (direction) {
+              remove(index);
+            },
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
       ),
     );
   }
